76431c97535d59456f994e633bba9fe909f798b7
Julio Foulquié <jfoulquie@gmail.com>
2015-03-07 18:48:04 +0100

    Improve TwitterApio and add new functions. Clean up the code a bit.

--- src/j3j5/TwitterApio.php
+++ src/j3j5/TwitterApio.php
@@ -1,8 +1,14 @@
 <?php
+
 /**
- * Documentation, License etc.
+ * TwitterApio
  *
- * @package twitterapio
+ * A wrapper to make easier to use Twitter's API with tmhOAuth library.
+ *
+ * @author Julio Foulquié
+ * @version 0.1.0
+ *
+ * 06 Mar 2015
  */
 
 namespace j3j5;
@@ -12,57 +18,80 @@ use tmhOAuth;
 class TwitterApio extends tmhOAuth {
 
 	protected $api_settings;
-	protected $config;
+	protected $general_config;
 
 	/**
-	 * Create an instance of the TwitterApi class
+	 * Create an instance of the TwitterApio class
 	 *
-	 * @param Array $settings The application settings to start the OAuth library
+	 * @param Array $settings The settings to start the OAuth library (consumers and tokens)
+	 * @param Array $config The application settings to start the OAuth library
+	 *
 	 * @return void
 	 */
 	public function __construct($settings = array(), $config = array()) {
 		require 'config.php';
-		$this->config = array_merge($general_config, $config);
+		$this->general_config = array_merge($general_config, $config);
 		parent::__construct(array_merge($twitter_settings, $settings));
 	}
 
 	/**
-	 * Do a request to the Twitter API
+	 * Do a GET request to the Twitter API
 	 *
 	 * @param string $slug The slug to retrieve from Twitter's API
 	 * @param string $parameters Optional parameters to set for the request
 	 *
 	 * @return Bool|Array A response array, or FALSE
+	 *
+	 * @author Julio Foulquié <jfoulquie@gmail.com>
 	 */
 	public function get($slug, $parameters = array()) {
-		$code = $this->request('GET', $this->url("{$this->config['api_version']}/$slug{$this->config['result_format']}"), $parameters);
+		$code = $this->request('GET', $this->url("{$this->general_config['api_version']}/$slug{$this->general_config['result_format']}"), $parameters);
 		return $this->response($code);
 	}
 
 	/**
-	 * Make a POST request to the Twitter API
+	 * Do a POST request to the Twitter API
 	 *
-	 * @param string $url The url to request from the Twitter API
+	 * @param string $slug The slug to retrieve from Twitter's API
 	 * @param string $parameters Optional parameters to set for the request
-	 * @return mixed A response array, or null
+	 *
+	 * @return Bool|Array A response array, or FALSE
+	 *
+	 * @author Julio Foulquié <jfoulquie@gmail.com>
 	 */
-	public function post($url, $parameters = array()) {
-		return $this->request('POST', $this->url($this->api_version . '/' . $url), $parameters);
+	public function post($slug, $parameters = array()) {
+		$code = $this->request('POST', $this->url("{$this->general_config['api_version']}/$slug{$this->general_config['result_format']}"), $parameters);
+		return $this->response($code);
 	}
 
 	/**
-	 * Make a DELETE request to the Twitter API
+	 * Get all possible tweets from a timeline endpoint.
+	 * This function returns an iterator so must be used with a function that implements the Iterator
+	 * interface
 	 *
-	 * @param string $url The url to request from the Twitter API
-	 * @param string $parameters Optional parameters to set for the request
-	 * @return mixed A response array, or null
+	 * @param String $slug The timeline endpoint to be used on the iterator
+	 * @param Array $arguments [Optional] Extra argument for the request
+	 *
+	 * @return TimelineIterator
+	 *
+	 * @author Julio Foulquié <jfoulquie@gmail.com>
 	 */
-	public function delete($url, $parameters = array()) {
-		return $this->request('DELETE', $this->url($this->api_version . '/' . $url), $parameters);
+	public function get_timeline($slug, $arguments = array()) {
+		return new TimelineIterator($this, $slug, $arguments);
 	}
 
-	public function cursorize_timeline($endpoint, $arguments = array()) {
-		return new FastTwitterAPI_Iterator_TimelineCursor($this, $endpoint, $arguments);
+	/**
+	 * Get all possible followers IDs for a given user
+	 *
+	 * @param Array $arguments Arguments for the request ('user_id' or 'screen_name' at least, but also 'count' or others are possible)
+	 *
+	 * @return FollowerIterator
+	 *
+	 * @author Julio Foulquié <jfoulquie@gmail.com>
+	 */
+	public function get_followers($arguments) {
+		$slug = 'followers/ids';
+		return new FollowerIterator($this, $slug, $arguments);
 	}
 
 	/**
@@ -108,13 +137,13 @@ class TwitterApio extends tmhOAuth {
 			///TODO: Log Error
 			return FALSE;
 		}
-		$json_output = $this->config['json_decode'] == 'array' ? TRUE : FALSE ;
+		$json_output = $this->general_config['json_decode'] == 'array' ? TRUE : FALSE ;
 
 		return json_decode($this->response['response'], $json_output);
 	}
 
 	/**
-	 * Return the error response when the requested values do not exist.
+	 * Return the error response when the requested values (or the endpoint) do not exist.
 	 * ex. a requested user does not exist or has been suspended, a tweet has been deleted...
 	 *
 	 * @return array
@@ -122,8 +151,9 @@ class TwitterApio extends tmhOAuth {
 	 * @author Julio Foulquié <jfoulquie@gmail.com>
 	 */
 	private function request_does_not_exist() {
+		///TODO :Log error
 		return array(
-			'code'		=> $code,
+			'code'		=> $this->response['code'],
 			'errors'	=> $this->response['response'],
 			'tts'		=> 0,
 		);
@@ -138,11 +168,12 @@ class TwitterApio extends tmhOAuth {
 	 * @author Julio Foulquié <jfoulquie@gmail.com>
 	 */
 	private function rate_limit() {
+		///TODO :Log error
 		return array(
-			'code'		=> $code,
+			'code'		=> $this->response['code'],
 			'errors'	=> $this->response['response'],
 			'tts'		=> isset($this->response['headers']['x-rate-limit-reset']) ? // Time To Sleep
-							$this->response['headers']['x-rate-limit-reset'] :
+							( $this->response['headers']['x-rate-limit-reset'] - time() ):
 							FALSE
 		);
 	}
@@ -155,100 +186,12 @@ class TwitterApio extends tmhOAuth {
 	 * @author Julio Foulquié <jfoulquie@gmail.com>
 	 */
 	private function general_error() {
-		return isset($this->response['response']) ? $this->response['response'] : FALSE
+		///TODO :Log error
+		return array(
+			'code'		=> $this->response['code'],
+			'errors'	=> $this->response['response'],
+			'tts'		=> 0,
+		);
 	}
 
-}
-
-/**
- * Iterator class to quickly iterate through an API
- */
-abstract class FastTwitterAPI implements \Iterator
-{
-	protected $api;
-	protected $endpoint;
-	protected $arguments;
-
-	public function __construct($api, $endpoint, $arguments) {
-		$this->api = $api;
-		$this->endpoint = $endpoint;
-		$this->arguments = $arguments;
-	}
-}
-
-class FastTwitterAPI_Iterator_TimelineCursor extends FastTwitterAPI {
-	private $since_id = -1;
-	private $latest_tweet_id;
-	private $max_id = -1;
-	private $first_tweet_id;
-	private $use_since_id = FALSE;
-
-	function __construct($api, $endpoint, $arguments) {
-		$this->use_since_id = TRUE;
-		if(isset($arguments['use_since_id'])) {
-			$this->use_since_id = (bool)$arguments['use_since_id'];
-		}
-		parent::__construct($api, $endpoint, $arguments);
-
-	}
-
-	function rewind() {
-		$this->since_id = $this->max_id = -1;
-	}
-
-	function current() {
-		$arguments = $this->arguments;
-		if($this->max_id > 0) {
-			$arguments['max_id'] = $this->max_id;
-		}
-
-		if($this->since_id > 0 && $this->use_since_id) {
-			$arguments['since_id'] = $this->since_id;
-		}
-
-		$i = 0;
-		do {
-			$resp = $this->api->get($this->endpoint, $arguments);
-		} while (is_array($resp)  && ++$i <= 3); // Retry 3 times if the response isn't present
-
-		if(is_array($resp)) {
-			$first_tweet = end($resp);
-			$latest_tweet = reset($resp);
-		} else {
-			$this->since_id = $this->max_id = 0;
-		}
-
-		if (!empty($first_tweet)) {
-			$this->max_id = $first_tweet['id'] -1;
-		} else {
-			$this->max_id = 0;
-		}
-
-		if(!empty($latest_tweet)) {
-			$this->since_id = $latest_tweet['id'];
-		}
-
-		if($this->since_id == $this->max_id) {
-			// This means, on the last request, the oldest and the latest were the same tweet, so stop here
-			$this->max_id = 0;
-		}
-		if(is_array($resp)) {
-			return $resp;
-		} else {
-			return FALSE;
-		}
-	}
-
-	function key() {
-		return $this->max_id;
-	}
-
-	function next() {
-		// For the next, max_id should already be set
-		return;
-	}
-
-	function valid() {
-		return ($this->max_id !== 0);
-	}
 }
