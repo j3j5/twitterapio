93651a18d7f55de8035baa5d40f0560f80f004c7
Julio Foulquie <jfoulquie@gmail.com>
2017-03-12 22:02:48 -0300

    Replace LICENSE with AGPLv3.

--- src/j3j5/TwitterApio.php
+++ src/j3j5/TwitterApio.php
@@ -1,14 +1,21 @@
 <?php
 
 /**
- * TwitterApio
- *
- * A wrapper to make easier to use Twitter's API with tmhOAuth library.
- *
- * @author Julio Foulquié
- * @version 0.1.0
- *
- * 06 Mar 2015
+    TwitterApio - A wrapper to make easier to use Twitter's API with tmhOAuth library.
+    Copyright (C) 2015  Julio Foulquie <jfoulquie@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace j3j5;
